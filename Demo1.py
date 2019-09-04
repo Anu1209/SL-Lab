66a6bd8c5bbd93f6d95290759acba9278c513cbb
Anu1209 <54896377+Anu1209@users.noreply.github.com>
2019-09-05 00:21:26 +0530

    Create Demo1.py

---
+++ Demo1.py
@@ -0,0 +1,15 @@
+print("Python does not need declaration")
+age=20
+name="John"
+height=6.62
+print('Age is:',age)
+print('name is:',name)
+print('height is:',height)
+age,height,name=30,6023,'raj'
+print('Age is:',age)
+print('name is:',name)
+print('height is:',height)
+age,nama,height='paul',62,7.43
+print('Age is:',age)
+print('name is:',name)
+print('height is:',height)
