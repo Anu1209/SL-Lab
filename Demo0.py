4e132144f15a974ec8bc098ea8f9fa5575a28b34
Anu1209 <54896377+Anu1209@users.noreply.github.com>
2019-09-04 23:15:27 +0530

    Update Demo0.py

--- Demo0.py
+++ Demo0.py
@@ -1,1 +1,19 @@
-print("Hello World")
+print("python has five different types of data types:-String,Lists,Tuple,Dictionary,Numbers")
+print("Demo string manipulation")
+mystr='hello world'
+print(mystr[0])
+print(mystr[1:5])
+print(mystr[2:])
+print("Length of the string",len(mystr))
+print("replacing",mystr.replace("world","World"))
+print("Can handle 3 types of data types:-int,float,complex")
+
+x=23
+y=6.52
+z=-1.3e100
+w=4j
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(w))
