01ce1cf1517bb6fdb760ca77cda1f8e7930248a0
Anu1209 <54896377+Anu1209@users.noreply.github.com>
2019-09-05 19:55:59 +0530

    Create AtomicDic.py

---
+++ AtomicDic.py
@@ -0,0 +1,17 @@
+list1 = ['H', 'Li', 'Li','He', 'H', 'Be','B', 'C', 'Li', 'N', 'N']
+# List
+print("\n list is")
+# Printing List
+print(list1)
+# Occurence
+Occurance1 = list1.count("N")
+Occurance2 = list1.count("He")
+Occurance3 = list1.count("Li")
+# Occurence print
+print("Number of occurance of N is",Occurance1)
+print("Number of occurance of He is",Occurance2)
+print("Number of occurance of Li is",Occurance3)
+# Occurance List
+print("List of Occurances\n")
+Occur=["N", Occurance1],[ 'He', Occurance2],[ 'Li', Occurance3]
+print(Occur)
