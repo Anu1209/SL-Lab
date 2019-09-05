a1884820ff02fdbea871ca96588643c5bf5289c0
Anu1209 <54896377+Anu1209@users.noreply.github.com>
2019-09-05 19:34:09 +0530

    Create classCreate.py

---
+++ classCreate.py
@@ -0,0 +1,28 @@
+class person:
+	x=0
+
+	def __init__(self,name,age):
+
+		self.name = name
+
+		self.age = age
+
+		person.x+=1
+
+p1=person("kol",20)
+
+p2=person("kjh",15)
+
+print ("Name of first person is :", p1.name)
+
+print ("Age of first person is :", p1.age)
+
+print ("Name of secondt person is :", p2.name)
+
+print ("Age of second person is :", p2.age)
+
+p2.age=10
+
+print("Changed age for second person is :",p2.age)
+
+print("no of persons :",person.x)
