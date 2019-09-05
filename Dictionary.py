b4b7c63455cbd5cad71d693109f10a57890c8c3f
Anu1209 <54896377+Anu1209@users.noreply.github.com>
2019-09-05 19:51:29 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,20 @@
+mydictionary1 = {
+    "name": "Archie",
+    "identity": "Student",
+    "age": 17,
+}
+mydictionary2 = {
+    "name": "Weatherbee",
+    "identity": "Principal",
+    "age": 52,
+}
+
+mydictionary3 = {
+    "name": "MGrundy",
+    "identity": "Teacher",
+    "age": 51
+}
+print('\n dictionaries')
+print(mydictionary1)
+print(mydictionary2)
+print(mydictionary3)
