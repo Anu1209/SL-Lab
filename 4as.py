9ea6380afdfb7a24f6a8c0e532369e3fafeae75a
Anu1209 <54896377+Anu1209@users.noreply.github.com>
2019-11-29 21:14:09 +0530

    Delete 4as.py

--- 4as.py
+++
@@ -1,8 +0,0 @@
-def AgeConverter(year):
-        age=2019-year
-        print(age)
-print('Enter the birth date')
-date=int(input('date'))
-month=int(input('month'))        
-year=int(input('year'))
-AgeConverter(year)
