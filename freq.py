aa4ad9b0ce3e5f49575147e0018fdb9707529035
Anu1209 <54896377+Anu1209@users.noreply.github.com>
2019-11-29 20:34:10 +0530

    Delete freq.py

--- freq.py
+++
@@ -1,9 +0,0 @@
-#counting the occcurence of each word in test
-from collections import Counter #collection is a library and Counter is being called
-def word_count(fname):
-	with open(fname) as f:
-		return Counter(f.read().split())
-		
-
-print("Number of words in a file:",word_count("test.txt"))
-
